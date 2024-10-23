a404c1059138b74905f6d85164eb3457c3dbcf69
suhassk-hash <sharmasuhas450@gmail.com>
2024-10-23 17:17:19 +0530

    initialize tailwind in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="w-12 h-12 bg-green-100"></div> 
       </header>
     </div>
   );
